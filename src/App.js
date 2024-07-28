1cefe6728b389b9808a3965abad1606a3821f31a
fpt-phongnx8 <phongnx8@fpt.com>
2024-07-28 10:59:42 +0900

    Change something

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cloud Mentor Pro 1
+          Cloud Mentor Pro 2
         </p>
         <a
           className="App-link"
